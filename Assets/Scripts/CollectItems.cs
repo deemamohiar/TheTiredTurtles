13182d23e7676c235a50fff711201b3fab9ab6a4
lisa <lisa-hanna@hotmail.com>
2022-05-19 20:01:55 -0400

    Modified key particle effect, centered level2title, and can now only enter ENTRANCE door when collecting key

--- Assets/Scripts/CollectItems.cs
+++ Assets/Scripts/CollectItems.cs
@@ -21,20 +21,20 @@ public class CollectItems : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         GameObject item = other.gameObject;
 
-        if(item.name == "Key1") {
+        if (item.name == "Key1") {
             isKeyCollected = true;
             // TODO Babriel: this is the part where u could call another script that u create that adds it to the HUD
             // Before destroying add it to HUD so u dont get error
             Destroy(item);
         }
-        else if(item.name == "Clue2") {
+        else if (item.name == "Clue2") {
             isClueCollected = true;
             //TODO Babriel: add clue to HUD before destroying (better if u create 
             // its own script and call the method or wtv here)
             Destroy(item);
         }
         
-        if(isKeyCollected && isClueCollected) {
+        if (isKeyCollected && isClueCollected) {
             OpenCloseDoor.isDoorUnlocked = true;
         }
     } 
