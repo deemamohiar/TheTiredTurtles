c9801739f875a9482842da7a6769e3a149b4cd6f
costakorres <ckorres11@gmail.com>
2022-05-20 02:06:01 -0400

    I tried to implement a better camera controller but it ended up being more complicated than expected

---
+++ Assets/Scripts/PickupCollectibles.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCollectibles : MonoBehaviour
+{
+    public AudioSource clueNarrationAudio;
+    // void OnTriggerEnter(Collider collision)
+    // {
+    //     if (collision.gameObject.tag.ToString() == "KeyTag")
+    //     {
+
+    //     }
+    // }
+}
