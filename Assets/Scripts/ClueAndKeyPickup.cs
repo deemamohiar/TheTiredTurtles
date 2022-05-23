eb2ea3df0455e1a0ee7364d94029989edbefc3be
costakorres <ckorres11@gmail.com>
2022-05-22 20:16:31 -0400

    changed ui interact icon, added in key features except bools for door. fixed bugs with the audio

--- Assets/Scripts/ClueAndKeyPickup.cs
+++ Assets/Scripts/ClueAndKeyPickup.cs
@@ -1,39 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ClueAndKeyPickup : MonoBehaviour
 {
     public AudioSource scrollPickupSound;
     public AudioSource keyPickupSound;
-    // public bool isInRange = false;
-    // public KeyCode interactKey;
-    // public UnityEvent interactAction;
-    void OnTriggerEnter(Collider collision)
+    // public GameObject scroll;
+    public TMP_Text scroll, keyNarrationText;
+    GameObject image, clue2Obj, key1Obj, keyNarrationBackground;
+    void Start()
     {
-        if (collision.gameObject.tag.ToString() == "Clue2Tag")
-        {
-            Debug.Log("in Range");
-            if(Input.GetKeyDown(KeyCode.F))
-            {
-                Debug.Log("picked up clue");
-                collision.gameObject.SetActive(false);
-                //show scroll for a few seconds
-                //scrollPickupSound.Play();
-            }
-
-        }
-        else if (collision.gameObject.tag.ToString() == "KeyTag")
-        {
-            Debug.Log("in Range");
-            if(Input.GetKeyDown(KeyCode.F))
-            {
-                Debug.Log("picked up key");
-                collision.gameObject.SetActive(false);
-                //show scroll for a few seconds
-                //scrollPickupSound.Play();
-            }
-        }
+        image = GameObject.Find("PapyrusPaper");
+        clue2Obj = GameObject.Find("Clue2");
+        key1Obj = GameObject.Find("Key1");
+        keyNarrationBackground = GameObject.Find("KeyNarrationBackground");
+        image.SetActive(false);
+        scroll.enabled = false;
+        keyNarrationBackground.SetActive(false);
+    }
+    public void PickupScroll()
+    {
+        clue2Obj.SetActive(false);
+        scrollPickupSound.Play();
+        StartCoroutine(ShowScroll());
+    }
+    public void PickupKey()
+    {
+        key1Obj.SetActive(false);
+        StartCoroutine(BottomNaration());
+        
+    }
+    public IEnumerator ShowScroll()
+    {
+        image.SetActive(true);
+        scroll.enabled = true;
+        yield return new WaitForSeconds(10.0f);
+        image.SetActive(false);
+        scroll.enabled = false;
+    }
+    public IEnumerator BottomNaration()
+    {
+        keyNarrationBackground.SetActive(true);
+        keyNarrationText.enabled = true;
+        yield return new WaitForSeconds(10.0f);
+        keyNarrationBackground.SetActive(false);
+        keyNarrationText.enabled = false;
     }
 }
--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -24,6 +24,7 @@ public class Interactor : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, interactableLayerMask))
         {
+            Debug.Log(hit.collider.name);
             if(hit.collider.GetComponent<Interactable>() != false)
             {
                 if(interactable == null || interactable.ID != hit.collider.GetComponent<Interactable>().ID)
