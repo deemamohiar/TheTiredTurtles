a0ed22792240e3057bd6c5ec90bc382ac8a1dc34
costakorres <ckorres11@gmail.com>
2022-05-27 01:56:10 -0400

    lights

--- Assets/Scripts/BookAndPaintingInteract.cs
+++ Assets/Scripts/BookAndPaintingInteract.cs
@@ -57,7 +57,8 @@ public class BookAndPaintingInteract : MonoBehaviour
     {
         if(paintings[index].GetComponent<Interactable>().enabled != true)
         {
-            BottomNarationWhenInteractingWithPainting("What am I doing?\nI should look for the clue...");
+            // BottomNarationWhenInteractingWithPainting("What am I doing?\nI should look for the clue...");
+            Debug.Log("Cant do anything");
         }
         else if(paintings[index].GetComponent<Interactable>().enabled == true)
         {
@@ -90,15 +91,15 @@ public class BookAndPaintingInteract : MonoBehaviour
         bookNarrationBackground.SetActive(false);
         bookNarrationText.enabled = false;
     }
-    public IEnumerator BottomNarationWhenInteractingWithPainting(string message)
-    {
-        bookNarrationBackground.SetActive(true);
-        bookNarrationText.SetText(message);
-        bookNarrationText.enabled = true;
-        yield return new WaitForSeconds(5.0f);
-        bookNarrationBackground.SetActive(false);
-        bookNarrationText.enabled = false;
-    }
+    // public IEnumerator BottomNarationWhenInteractingWithPainting(string message)
+    // {
+    //     bookNarrationBackground.SetActive(true);
+    //     bookNarrationText.SetText(message);
+    //     bookNarrationText.enabled = true;
+    //     yield return new WaitForSeconds(5.0f);
+    //     bookNarrationBackground.SetActive(false);
+    //     bookNarrationText.enabled = false;
+    // }
     public void EndLevel()
     {
         scrollPickupEffect.Play();
